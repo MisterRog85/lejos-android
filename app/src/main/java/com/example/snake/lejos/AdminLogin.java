bc11da9c90ca231a2f5fb20d664e2feb2473e6fb
William Tomas <william.tomas.85@gmail.com>
2019-04-06 11:22:10 +0200

    v1.1
    accès zone admin

--- app/src/main/java/com/example/snake/lejos/AdminLogin.java
+++ app/src/main/java/com/example/snake/lejos/AdminLogin.java
@@ -4,33 +4,32 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class AdminLogin extends AppCompatActivity {
-    Context ctx;
+    //Context ctx;
     String password = "admin";
+
+    public EditText mdp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_login);
 
-        final EditText mdp = (EditText) findViewById(R.id.password);
-        final Button valider = (Button) findViewById(R.id.valider);
+        mdp = (EditText) findViewById(R.id.password);
+    }
 
-
-        valider.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mdp.toString() == password){
-                    Intent intent = new Intent(ctx, AdminSettings.class);
-                    startActivity(intent);
-                } else {
-                    Toast.makeText(ctx, "Erreur mot de passe", Toast.LENGTH_LONG).show();
-                }
-            }
-        });
+    public void connexionAdmin (View view) {
+        Log.d("log1 le mdp lu", mdp.getText().toString());
+        if (mdp.getText().toString().equals(password)){
+            Intent intent = new Intent(this, AdminSettings.class);
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, "Erreur mot de passe", Toast.LENGTH_LONG).show();
+        }
     }
 }
